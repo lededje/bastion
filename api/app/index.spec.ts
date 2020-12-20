9fd11a171c81c5db56a621579369773f808ee8cd
Miles Spoor <git@likeminded.io>
2020-12-20 20:08:23 +0000

    GH-5: Added postgres and updated heathcheck

--- api/app/index.spec.ts
+++ api/app/index.spec.ts
@@ -1,19 +1,25 @@
 import request from 'supertest'
+import knex from '../services/knex'
 
 import app, { HealthResponse } from './';
 
-describe('GET /healthcheck', () => {
+describe('GET /', () => {
   it('responds with json', (done) => {
     request(app)
-      .get('/healthcheck')
+      .get('/')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, (err, resp) => {
         if(err) done(err);
         expect(resp.body).toEqual<HealthResponse>({
           healthy: true,
+          dbConnection: true,
         })
         done();
       });
   });
 });
+
+afterAll(() => {
+  knex.destroy()
+})
